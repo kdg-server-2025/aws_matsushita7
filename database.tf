d7c4b6f5cd76b6d3eaf7496880832b367838a097
matsushita7 <yutaro.matsushita0402@gmail.com>
2025-07-27 00:10:08 +0900

    disable:接続が確認できたため、RDSのリソースを一時停止。

--- database.tf
+++ database.tf
@@ -14,36 +14,36 @@
 
 
 
-resource "aws_db_instance" "postgres" {
-  identifier              = "macha-db"
-  db_name                 = "postgres"
-  allocated_storage       = 20
-  storage_type            = "gp2"
-  engine                  = "postgres"
-  engine_version          = "17.4"
-  instance_class          = "db.t4g.micro"
-  password                = var.db_password
-  username                = var.db_username
-  backup_retention_period = 0
+# resource "aws_db_instance" "postgres" {
+#   identifier              = "macha-db"
+#   db_name                 = "postgres"
+#   allocated_storage       = 20
+#   storage_type            = "gp2"
+#   engine                  = "postgres"
+#   engine_version          = "17.4"
+#   instance_class          = "db.t4g.micro"
+#   password                = var.db_password
+#   username                = var.db_username
+#   backup_retention_period = 0
   
-  multi_az                = false
+#   multi_az                = false
 
-  # 追加の費用が掛かることを防止するため snapshot と Performance Insights は無効にする
-  skip_final_snapshot     = true
-  copy_tags_to_snapshot        = false
-  storage_encrypted            = false
-  performance_insights_enabled = false
+#   # 追加の費用が掛かることを防止するため snapshot と Performance Insights は無効にする
+#   skip_final_snapshot     = true
+#   copy_tags_to_snapshot        = false
+#   storage_encrypted            = false
+#   performance_insights_enabled = false
 
-  # 
-  vpc_security_group_ids  = [aws_security_group.rds_enable.id]
-  publicly_accessible = true
+#   # 
+#   vpc_security_group_ids  = [aws_security_group.rds_enable.id]
+#   publicly_accessible = true
 
-  tags = {
-    Name = "PostgreSQL 17.4 DB"
-  }
-}
+#   tags = {
+#     Name = "PostgreSQL 17.4 DB"
+#   }
+# }
 
-output "rds_endpoint" {
-  description = "RDS のエンドポイント"
-  value       = aws_db_instance.postgres.address
-}
+# output "rds_endpoint" {
+#   description = "RDS のエンドポイント"
+#   value       = aws_db_instance.postgres.address
+# }
